19d3f61e6265d786fa93839b754a83025d5b5cf2
kevdome3000 <kbassjr@gmail.com>
2024-11-03 15:41:19 -0800

    Update storage provider constants
    Renamed GROUP_STORAGE_PROVIDER, USER_STORAGE_PROVIDER, and MESSAGE_STORAGE_PROVIDER to use "ORLEANSR" prefix instead of "ORGNALR". This change ensures consistency with the naming conventions used within the project.

--- src/OrleansR.Backplane.GrainImplementations/Constants.cs
+++ src/OrleansR.Backplane.GrainImplementations/Constants.cs
@@ -2,7 +2,7 @@ namespace OrleansR.Backplane.GrainImplementations;
 
 public static class Constants
 {
-    public const string GROUP_STORAGE_PROVIDER = "ORGNALR_GROUP_STORAGE";
-    public const string USER_STORAGE_PROVIDER = "ORGNALR_USER_STORAGE";
-    public const string MESSAGE_STORAGE_PROVIDER = "ORGNALR_MESSAGE_STORAGE";
+    public const string GROUP_STORAGE_PROVIDER = "ORLEANSR_GROUP_STORAGE";
+    public const string USER_STORAGE_PROVIDER = "ORLEANSR_USER_STORAGE";
+    public const string MESSAGE_STORAGE_PROVIDER = "ORLEANSR_MESSAGE_STORAGE";
 }
